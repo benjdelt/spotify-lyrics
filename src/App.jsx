b09a1946f8a2234a549bd2181d59975fa32db247
Ben Deltenre <benjdelt@gmail.com>
2019-04-27 15:43:18 -0700

    Set up main bootstrap grid

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-    <h1>Spotify Lyrics</h1>
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-8" id="col-2">
+            Main Content
+          </div>
+          <div className="col-sm-4 order-sm-first" id="col-1">
+            Navigation
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
