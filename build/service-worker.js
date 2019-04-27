cfe68925f9007674f18fea79b2a6b92ce056cbd0
Ben Deltenre <benjdelt@gmail.com>
2019-04-27 10:19:29 -0700

    Cleaned default code and added Bootstrap

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.380adf610922866d450d1a7155a60b54.js"
+  "/precache-manifest.1b64836904594b980545b39f9c26de20.js"
 );
 
 self.addEventListener('message', (event) => {
