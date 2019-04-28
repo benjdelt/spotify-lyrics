b97e98d805836539916fb09e60cb8a4cd33f97b3
Ben Deltenre <benjdelt@gmail.com>
2019-04-28 12:48:38 -0700

    Added React Bootstrap, fake carousel and user

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,65 @@
 import React from 'react';
 import './App.css';
+import { Carousel } from 'react-bootstrap'
 
 function App() {
   return (
     <div className="container-fluid h-100">
       <div className="row h-100">
-        <main className="col-sm-9">
+        <main className="col-sm-10">
           Main Content
         </main>
-        <header className="col-sm-3 order-sm-first">
-          <h1 id="logo">Spotify Lyrics</h1>
-          <br/>
+        <header className="col-sm-2 order-sm-first">
+          <ul className="nav flex-column">
+            <li className="nav-item">
+              <h1 id="logo">Spotify Lyrics</h1>
+            </li>
+            <li className="nav-item">
+              <Carousel>
+                <Carousel.Item>
+                  <img
+                    className="d-block w-100"
+                    src="https://picsum.photos/200?random=1"
+                    alt="First cover"
+                  />
+                  <Carousel.Caption>
+                    <h3>First cover label</h3>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                  </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                  <img
+                    className="d-block w-100"
+                    src="https://picsum.photos/200?random=2"
+                    alt="Third cover"
+                  />
+
+                  <Carousel.Caption>
+                    <h3>Second cover label</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                  </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                  <img
+                    className="d-block w-100"
+                    src="https://picsum.photos/200?random=3"
+                    alt="Third cover"
+                  />
+
+                  <Carousel.Caption>
+                    <h3>Third cover label</h3>
+                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                  </Carousel.Caption>
+                </Carousel.Item>
+              </Carousel>
+            </li>
+            <li className="nav-item">
+              <div className="user">
+                <img src="https://picsum.photos/30" alt=""/>
+                <h2>User</h2>
+              </div>
+            </li>
+          </ul>
         </header>
       </div>
     </div>
