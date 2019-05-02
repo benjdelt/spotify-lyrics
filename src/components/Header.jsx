d60b3fcabab5e8627b97f208dd0018cda7c260eb
Ben Deltenre <benjdelt@gmail.com>
2019-05-02 10:27:04 -0700

    Rendered selected tracks

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,8 @@ import { Button, Dropdown } from 'react-bootstrap';
 import { 
   getLoggedIn,
   fetchUser,
-  fetchRecentlyPlayed
+  fetchRecentlyPlayed,
+  selectTrack
 } from '../redux/actions';
 
 import Login from './Login';
@@ -19,12 +20,20 @@ class Header extends Component {
     this.props.fetchRecentlyPlayed();
   }
 
+  handleClick(track, index) {
+    if (track === "current") {
+      this.props.selectTrack(this.props.nowPlaying);
+    } else {
+      this.props.selectTrack(this.props.recentlyPlayed[index]);
+    }
+  }
+
   render() {
     return (
         <header className="col-sm-2 order-sm-first">
         <ul className="nav flex-column">
           <li className="nav-item">
-            <a href="#" className="logo">
+            <a href="/" className="logo">
               <div className="image-cont">
                 <img src="/images/inverted-logo.png" alt="logo" className="avatar img-responsive"/>
               </div>
@@ -34,7 +43,7 @@ class Header extends Component {
           {this.props.loggedIn && (
             <div>
               <li className="nav-item">
-                <Button variant="link">
+                <Button variant="link" onClick={() => this.handleClick("current")}>
                   <i className="fas fa-volume-up"></i><span> Currently Playing</span>
                 </Button>
               </li>
@@ -45,9 +54,11 @@ class Header extends Component {
                   </Dropdown.Toggle>
 
                   <Dropdown.Menu>
-                    {this.props.recentlyPlayed.map(track => {
+                    {this.props.recentlyPlayed.map((track, index) => {
                       return (
-                        <Dropdown.Item href="#/action-1">{track.name}</Dropdown.Item>
+                        <Dropdown.Item key={index} onClick={() => this.handleClick("recent", index)} >
+                          {track.name}
+                        </Dropdown.Item>
                       )
                     })}
                   </Dropdown.Menu>
@@ -74,11 +85,13 @@ class Header extends Component {
 const mapStateToProps = state => ({
   loggedIn: state.loggedIn,
   user: state.user,
-  recentlyPlayed: state.recentlyPlayed
+  recentlyPlayed: state.recentlyPlayed,
+  selectedTrack: state.selectedTrack
 })
 
 export default connect(mapStateToProps, { 
   getLoggedIn,
   fetchUser,
-  fetchRecentlyPlayed
+  fetchRecentlyPlayed,
+  selectTrack
 })(Header);
