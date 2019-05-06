ed9bd7f0235f23d19038e3bfbfc217c557bdbd76
Ben Deltenre <benjdelt@gmail.com>
2019-05-06 14:40:28 -0700

    Updated Redux devtools configuration

--- src/App.jsx
+++ src/App.jsx
@@ -10,13 +10,18 @@ import Main from './components/Main';
 import Header from './components/Header'
 
 
-const store = createStore(
-  reducer, 
-  compose(
-    applyMiddleware(reduxLogger, reduxThunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+const composeEnhancers =
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?   
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+      // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+    }) : compose;
+
+const enhancer = composeEnhancers(
+  applyMiddleware(reduxLogger, reduxThunk),
+  // other store enhancers if any
 );
+const store = createStore(reducer, enhancer);
 
 
 function App() {
