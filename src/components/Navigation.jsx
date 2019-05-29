1d4977568c5cb12ba03cf23237439b7bd41f739d
Ben Deltenre <benjdelt@gmail.com>
2019-05-29 15:27:19 -0700

    Fixed mobile styling

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -54,7 +54,7 @@ function Navigation(props) {
           </li>
           <li className="nav-item">
             <a href="http://github.com/benjdelt/spotify-lyrics" target="_blank" rel="noopener noreferrer" class="btn btn-link">
-              <i class="fab fa-github"></i> Github Repo
+              <i class="fab fa-github"></i><span> Github Repo</span>
             </a>
           </li>
         </div>   
